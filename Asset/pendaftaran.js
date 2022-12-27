eddbd2c8281a4625d5a0078de66081d380846a78
Fero Ezar Felix <ferofelix123@gmail.com>
2022-12-27 14:50:10 +0700

    27 dec

--- Asset/pendaftaran.js
+++ Asset/pendaftaran.js
@@ -1,10 +1,10 @@
 // function masukan(){
-//     var n = document.forms['bolot']['nama'].value;
-//     var c = document.forms['bolot']['tempat'].value;
-//     var e = document.forms['bolot']['email'].value;
-//     var p = document.forms['bolot']['JK'].value;
-//     var o = document.forms['bolot']['hobby'].value;
-//     var a = document.forms['bolot']['alamat'].value;        
+//     var n = document.forms['form']['nama'].value;
+//     var c = document.forms['form']['tempat'].value;
+//     var e = document.forms['form']['email'].value;
+//     var p = document.forms['form']['JK'].value;
+//     var o = document.forms['form']['hobby'].value;
+//     var a = document.forms['form']['alamat'].value;        
                                             
 //     var tabel = document.getElementById("databel");
 //     var baris = tabel.insertRow(1);
@@ -44,6 +44,6 @@ var yyyy = today.getFullYear();
 var tanggal
 
 today = mm + dd + yyyy;
-tanggal = document.getElementById("daftar").value = "";
+daftar = document.getElementById("daftar").value = "MHS"+today;
 // document.write(today);
 }
